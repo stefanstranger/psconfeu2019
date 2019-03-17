8b7c600fb24222a615431e8c4f13a8d6f6d8679d
Stefan Stranger <stefan@stranger.nl>
2019-03-17 14:57:54 +0100

    Added fail test for when Group is not found

--- Prototype/AAD/Module/ManageAADGroup/Tests/Get-AADGroup.tests.ps1
+++ Prototype/AAD/Module/ManageAADGroup/Tests/Get-AADGroup.tests.ps1
@@ -47,6 +47,41 @@ Describe -Name 'Test Get-AzureAadGroup Function' -Fixture {
         Assert-MockCalled Invoke-RestMethod -ParameterFilter {'https://graph.microsoft.com/v1.0/groups?$filter=startswith(displayName,''PesterDemo'')' -eq $uri} -ModuleName ManageAadGroup -Exactly 1 -Scope It
     }
 }
+
+
+#region Test Web Exceptions
+Describe -Name 'Test Get-AzureAadGroup Function Exceptions' -fixture {
+
+    It -name 'Should fail when Group is not found' -test {
+
+        # Mock Invoke-RestMethod for retrieving token
+        Mock Invoke-RestMethod {return @{'access_token' = '1234567'}} -ParameterFilter {Write-Verbose ('Mock Invoke-RestMethod Uri filter {0} for token' -f $uri); 'https://login.microsoftonline.com/12345/oauth2/v2.0/token' -eq $uri } -ModuleName ManageAadGroup -Verifiable
+            
+        # Mock Invoke-RestMethod for not existing Group.
+        Mock Invoke-RestMethod {     
+            $Exception = [System.Management.Automation.RuntimeException]::new()
+            $Response = [PSCustomObject]@{ 
+                Message = 'Error Message AAD Group PesterDemo not found'
+            }
+            $Exception | Add-Member -Name Response  -MemberType NoteProperty -Value $Response -Force
+            Throw $Exception
+        } -ParameterFilter {Write-Verbose ('Mock Invoke-RestMethod Uri filter {0}' -f $uri); 'https://graph.microsoft.com/v1.0/groups?$filter=startswith(displayName,''PesterDemo'')' -eq $Uri} -Module ManageAadGroup -Verifiable 
+    
+        #Parameters
+        $params = @{
+            'DisplayName'  = 'PesterDemo'
+            'ClientId'     = '12345'
+            'ClientSecret' = '12345'
+            'TenantId'     = '12345'      
+        }
+        {Get-AadGroup @params} | Should Throw
+
+        # Test if Mock is being called
+        Assert-MockCalled Invoke-RestMethod -ParameterFilter {'https://login.microsoftonline.com/12345/oauth2/v2.0/token' -eq $uri} -ModuleName ManageAadGroup -Exactly 1 -Scope It
+
+        Assert-MockCalled Invoke-RestMethod -ParameterFilter {'https://graph.microsoft.com/v1.0/groups?$filter=startswith(displayName,''PesterDemo'')' -eq $uri} -ModuleName ManageAadGroup -Exactly 1 -Scope It
+    }
+}
 #endregion
 
 
