0adf77ef8a2dacdd7e19dda4cbc346b317b7d014
Stefan Stranger <stefan@stranger.nl>
2019-03-29 10:02:46 +0100

    Created demo script for ManageAADGroup PowerShell module

---
+++ Prototype/Demo/Demo-4-ManageAADGroup-Module.ps1
@@ -0,0 +1,51 @@
+<#
+    ManageAAD Group PowerShell module is a wrapper module on the Graph API for managing Active Directory Groups.
+#>
+
+#region variables
+$Tenantid = '496f0b27-4fa4-4c3d-8bbe-19c4b6875c81' #sstranger
+$ClientID = 'a6e00d89-360e-40c6-a7de-3ad29d733fc7' #ApplicationID
+$ClientSecret = "$env:psconfeu"
+$GroupName = 'PSConfEu-Demo-Group'
+#endregion
+
+#region Import ManageAADGroup Module
+Import-Module ..\AAD\Module\ManageAADGroup -verbose
+#endregion
+
+#region Check if Group Exists
+$params = @{
+    'DisplayName'  = $GroupName
+    'ClientID'     = $ClientID
+    'ClientSecret' = $ClientSecret
+    'TenantID'     = $Tenantid
+    'Verbose'      = $true
+}
+Get-AADGroup @params
+#endregion
+
+#region Create AAD Security Group
+$params = @{
+    'DisplayName'       = $GroupName
+    'Description'       = 'PSConfEu Demo Group'
+    'MailNickName'      = 'psconfeudemogroup'
+    'UserPrincipalName' = 'johndoe@sstranger.onmicrosoft.com'
+    'Members'           = 'janedoe@sstranger.onmicrosoft.com'
+    'ClientID'          = $ClientID
+    'ClientSecret'      = $ClientSecret
+    'TenantID'          = $Tenantid
+    'Verbose'           = $true
+}
+New-AADGroup @params
+#endregion
+
+#region remove AAD Group
+$params = @{
+    'DisplayName'  = $GroupName
+    'ClientID'     = $ClientID
+    'ClientSecret' = $ClientSecret
+    'TenantID'     = $Tenantid
+    'Verbose'      = $true
+}
+Remove-AADGroup @params
+#endregion
