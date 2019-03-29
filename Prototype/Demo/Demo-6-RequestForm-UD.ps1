c3288c928b8a5c56144de79d6e47abd9ab9ea428
Stefan Stranger <stefan@stranger.nl>
2019-03-29 15:17:41 +0100

    Front end created with UD

---
+++ Prototype/Demo/Demo-6-RequestForm-UD.ps1
@@ -0,0 +1,38 @@
+<#
+    Demo Request form calling REST API
+
+    Remark: Make sure the REST API is running
+#>
+
+#region Create new Request Dashboard
+$RequestDashboard = New-UDDashboard -Title 'Request Group' -Content {
+    New-UDInput -Title 'Request form for Azure Active Directory Groups' -Id 'Form' -Content {
+        New-UDInputField -Type 'textbox' -Name 'UserPrincipalName' -Placeholder 'Email Address'
+        New-UDInputField -Type 'textbox' -Name 'DisplayName' -Placeholder 'Group Name'
+        New-UDInputField -Type 'textbox' -Name 'Members' -Placeholder 'Member email address'
+    } -Endpoint {
+        param($UserPrincipalName, $DisplayName, $Members)
+        #region Call REST API to Create Group
+        $body = @{
+            'DisplayName'       = $DisplayName
+            'Description'       = 'PSConfEu Demo Group'
+            'MailNickName'      = 'psconfeudemogroup'
+            'UserPrincipalName' = $UserPrincipalName
+            'Members'           = $Members
+        }
+      
+        $Result = Invoke-RestMethod -Uri http://localhost:8080/api/newgroup -Method POST -Body $body
+
+        New-UDInputAction -Toast ($Result | convertto-json) -Duration 10000
+        #endregion
+    }    
+}
+#endregion
+
+#region Start Dashboard
+Start-UDDashboard -port 80 -Dashboard $RequestDashboard -Name Request
+#endregion
+
+#region Stop Dashboard
+Stop-UDDashboard -Name Request
+#endregion
