a27ec66e70f932068402783beed712823beea914
Stefan Stranger <stefan@stranger.nl>
2019-03-29 12:13:19 +0100

    New working AAD Group UD Demo script

--- Prototype/Demo/Demo-5-AADGroup-UD.ps1
+++ Prototype/Demo/Demo-5-AADGroup-UD.ps1
@@ -11,7 +11,7 @@ $Endpoints = @()
 #endregion
 
 #region Create a REST API endpoint for Getting AAD Group info
-$Endpoints += New-UDEndpoint -Url "/getgroup" -Method "POST" -Endpoint {
+$Endpoints += New-UDEndpoint -Url "/getgroup" -Method "GET" -Endpoint {
     param($DisplayName)
 
     #region variables
@@ -57,7 +57,6 @@ $Endpoints += New-UDEndpoint -Url "/getgroup" -Method "POST" -Endpoint {
 }
 #endregion
 
-
 #region Create REST API Endpoint for Creating an AAD Group
 $Endpoints += New-UDEndpoint -Url "/newgroup" -Method "POST" -Endpoint {
     param($DisplayName, $Description, $MailNickName, $UserPrincipalName, $Members)
@@ -83,7 +82,8 @@ $Endpoints += New-UDEndpoint -Url "/newgroup" -Method "POST" -Endpoint {
             'ClientSecret'      = $ClientSecret
             'TenantID'          = $Tenantid
         }
-        New-AADGroup @params | ForEach-Object { [PSCustomObject]@{ 
+        New-AADGroup @params | Foreach-Object { 
+            [PSCustomObject]@{ 
                 id              = $_.id; 
                 createdDateTime = $_.createdDateTime
                 description     = $_.description
@@ -92,8 +92,8 @@ $Endpoints += New-UDEndpoint -Url "/newgroup" -Method "POST" -Endpoint {
                 mailNickName    = $_.mailNickName
                 renewedDateTime = $_.renewedDateTime
                 securityEnabled = $_.securityEnabled
-            } }  | ConvertTo-Json
-        #endregion
+            } 
+        } | ConvertTo-Json
     }
     Catch {
         $Exception = [System.Net.WebException]::new()
@@ -106,19 +106,76 @@ $Endpoints += New-UDEndpoint -Url "/newgroup" -Method "POST" -Endpoint {
         $Exception
     }
 }
+#endregion
 
+#region Create REST API Endpoint for Removing an AAD Group
+$Endpoints += New-UDEndpoint -Url "/removegroup" -Method "POST" -Endpoint {
+    param($DisplayName)
 
+    #region variables
+    $Tenantid = '496f0b27-4fa4-4c3d-8bbe-19c4b6875c81' #sstranger
+    $ClientID = 'a6e00d89-360e-40c6-a7de-3ad29d733fc7' #ApplicationID
+    $ClientSecret = "$env:psconfeu"
+    #endregion
+
+    #region Import Custom ManageAADGroup Module
+    Import-Module C:\Users\stefstr\Documents\GitHub\psconfeu2019\Prototype\Module\ManageAADGroup
+    #endregion
+
+    Try {
+        $params = @{
+            'DisplayName'  = $DisplayName
+            'ClientID'     = $ClientID
+            'ClientSecret' = $ClientSecret
+            'TenantID'     = $Tenantid
+        }
+        Remove-AADGroup @params | Foreach-Object { 
+            [PSCustomObject]@{ 
+                Code    = $_.Code; 
+                Message = $_.Message
+            } 
+        } | ConvertTo-Json
+    }
+    Catch {
+        $Exception = [System.Net.WebException]::new()
+        $Response = [PSCustomObject]@{ 
+            StatusCode = [PSCustomObject]@{
+                Value__ = 404
+            }
+        }
+        $Exception | Add-Member -Name Response  -MemberType NoteProperty -Value $Response -Force
+        $Exception
+    }
+}
 #endregion
 
 #region start Endpoints
 Start-UDRestApi -Endpoint $Endpoints
 #endregion
 
+#region Call REST API to Create Group
+$body = @{
+    'DisplayName'       = 'PSConfEu-Demo-Group'
+    'Description'       = 'PSConfEu Demo Group'
+    'MailNickName'      = 'psconfeudemogroup'
+    'UserPrincipalName' = 'johndoe@sstranger.onmicrosoft.com'
+    'Members'           = 'janedoe@sstranger.onmicrosoft.com'
+}
+Invoke-RestMethod -Uri http://localhost:80/api/newgroup -Method POST -Body $body -OutVariable Result
+#endregion
+
 #region Call REST API to get Group
 $body = @{
     'DisplayName' = 'PSConfEu-Demo-Group'
 }
-Invoke-RestMethod -Uri http://localhost:80/api/getgroup -Method POST -Body $body -OutVariable Result
+Invoke-RestMethod -Uri http://localhost:80/api/getgroup -Method GET -Body $body -OutVariable Result
+#endregion
+
+#region Call REST API to remove Group
+$body = @{
+    'DisplayName' = 'PSConfEu-Demo-Group'
+}
+Invoke-RestMethod -Uri http://localhost:80/api/removegroup -Method POST -Body $body -OutVariable Result
 #endregion
 
 #region Stop UD REST API
