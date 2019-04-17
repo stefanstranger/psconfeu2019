8bbd93cefd06646f059e6ce6e01bb1239cd7bfb0
Stefan Stranger <stefan@stranger.nl>
2019-04-17 14:48:19 +0200

    Removed token request

--- Prototype/AzureFunction/GetGroup/run.ps1
+++ Prototype/AzureFunction/GetGroup/run.ps1
@@ -14,7 +14,31 @@ if (-not $DisplayName) {
 
 if ($DisplayName) {
     $status = [HttpStatusCode]::OK
-       
+     
+    <#
+    #region Authenticate
+    $TokenEndpoint = ('https://login.microsoftonline.com/{0}/oauth2/v2.0/token' -f $env:tenantid )
+
+    $Body = @{
+        'client_id'     = $env:ClientID
+        'grant_type'    = 'client_credentials'
+        'client_secret' = $env:ClientSecret
+        'scope'         = 'https://graph.microsoft.com/.default'
+    }
+
+    $params = @{
+        ContentType = 'application/x-www-form-urlencoded'
+        Headers     = @{'accept' = 'application/json' }
+        Body        = $Body
+        Method      = 'Post'
+        URI         = $TokenEndpoint
+    }
+
+    $Body = Invoke-RestMethod @params
+    #endregion
+    #>
+
+
     #region Check if Group Exists
     $params = @{
         'DisplayName'  = $DisplayName
@@ -23,16 +47,17 @@ if ($DisplayName) {
         'TenantID'     = $env:Tenantid
         'Verbose'      = $true
     }
-    $body = Get-AADGroup @params
+    $Body = Get-AADGroup @params
     #endregion
+
 }
 else {
-    $status = [HttpStatusCode]::BadRequest
-    $body = "Please pass a DisplayName for Azure Active Directory Group on the query string or in the request body."
+    $Status = [HttpStatusCode]::BadRequest
+    $Body = "Please pass a DisplayName for Azure Active Directory Group on the query string or in the request body."
 }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-        StatusCode = $status
-        Body       = $body
+        StatusCode = $Status
+        Body       = $Body
     })
