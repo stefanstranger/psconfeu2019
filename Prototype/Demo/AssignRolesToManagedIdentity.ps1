d1fd4d19c87bd3f65e415f8d8b6e26101ad6bfc7
Stefan Stranger <stefan@stranger.nl>
2019-04-18 16:59:21 +0200

    Assign Roles to Managed Identiy

---
+++ Prototype/Demo/AssignRolesToManagedIdentity.ps1
@@ -0,0 +1,181 @@
+<#
+    The Managed Identity created for the Azure Function App needs Microsoft Graph App Role permissions.
+    Application Permissions:
+        - Read all groups
+        - Read and write all groups
+        - Read directory data
+    Delegated Permissions:
+        - Read and write all groups
+        - View your basic profile
+#>
+
+#region variables
+$Tenantid = '496f0b27-4fa4-4c3d-8bbe-19c4b6875c81' #sstranger
+$tenantdomainName = 'sstranger.onmicrosoft.com'
+$apiversion = '1.6'
+#endregion
+
+#region Connect with Azure Subscription
+Add-AzAccount -Tenant $Tenantid
+#endregion
+
+#region retrieve current context
+Write-Output -InputObject ('Retrieve current context')
+$Context = Get-AzContext
+$Context | convertto-json
+#endregion
+
+#region retrieve Access Token
+$User = Get-AzADUser -StartsWith $($Context.Account.Id).Split('.')[0]
+$Context.TokenCache
+$cache = $Context.TokenCache
+$cacheItems = $cache.ReadItems()
+$token = ($cacheItems | Where-Object { $_.Resource -eq "https://graph.windows.net/" })
+$AccessToken = $token.AccessToken
+
+write-output -InputObject ("Token: {0}`n AccountId: {1}`n TenantId: {2}" -f $AccessToken, $User.DisplayName, $Context.Tenant.Id )
+#endregion
+
+#region get AppRoles for Graph API
+function Get-ServicePrincipal {
+    [CmdletBinding()]
+    param (
+        [string]$Token,
+        [string]$TenantDomain,
+        [string]$ApiVersion = '1.6'
+    )
+    
+    begin {
+        #region variables
+        $Uri = ('https://graph.windows.net/{0}/{1}?api-version={2}' -f $tenantDomain, 'servicePrincipals', $apiversion)
+        #endregion
+    }
+    
+    process {
+
+        $params = @{
+            ContentType = 'application/json'
+            Headers     = @{
+                'authorization' = "Bearer $($Token)"
+            }
+            Method      = 'Get'
+            URI         = $Uri 
+        }
+          
+        return (Invoke-RestMethod @params).value
+        
+    }
+}
+
+Get-ServicePrincipal -Token $AccessToken -TenantDomain $tenantdomainName -ApiVersion $apiversion | Where-Object { $_.AppDisplayName -eq 'Microsoft Graph' } -OutVariable Graph
+$Graph.AppRoles
+#endregion
+
+#region Get Managed Identity Object ID
+Get-ServicePrincipal -Token $AccessToken -TenantDomain $tenantdomainName -ApiVersion $apiversion | Where-Object { $_.DisplayName -eq 'psconfeu2019' } -OutVariable ManagedIdentity
+#endregion
+
+#region Get Managed Identity Role Assignments
+function Get-MIAppRoleAssignment {
+    [CmdletBinding()]
+    param (
+        [string]$Token,
+        [string]$TenantId,
+        [string]$ApiVersion = '1.6',
+        [string]$ObjectId
+    )
+    
+    begin {
+        #region variables
+        $Uri = ('https://graph.windows.net/{0}/{1}/{2}/appRoleAssignments?api-version={3}' -f $tenantId, 'servicePrincipals', $ObjectId, $apiversion)
+        #endregion
+        Write-Verbose -Message ('Uri: {0}' -f $Uri)
+    }
+    
+    process {
+
+        $params = @{
+            ContentType = 'application/json'
+            Headers     = @{
+                'authorization' = "Bearer $($Token)"
+            }
+            Method      = 'Get'
+            URI         = $Uri 
+        }
+          
+        return (Invoke-RestMethod @params)
+        
+    }
+}
+
+Get-MIAppRoleAssignment -Token $AccessToken -TenantId $tenantid -ObjectId $($ManagedIdentity.ObjectId) -OutVariable result
+$result | Select-Object -ExpandProperty value
+#endregion
+
+#region Retrieve Application and Delegated Graph API Permissions
+$ApplicationPermissions = $Graph.AppRoles | Where-Object { $_.DisplayName -eq 'Read all groups' -or $_.DisplayName -eq 'Read and write all groups' -or $_.DisplayName -eq 'Read directory data' }
+$DelegatedPermissions = $Graph.oauth2Permissions | Where-Object { $_.userconsentdisplayname -eq 'Read and write all groups' -or $_.userconsentdisplayname -eq 'View your basic profile' }
+#endregion
+
+#region Add Graph API Role permissions for Managed Identity
+function Set-MIAppRoleAssignment {
+    [CmdletBinding()]
+    param (
+        # ObjectID for Service Principal.
+        [Parameter(Mandatory = $true,
+            ValueFromPipeline = $true)]
+        [string]$ObjectId,
+
+        # RoleAssignmentID.
+        [Parameter(Mandatory = $true,
+            ValueFromPipeline = $true)]
+        [string]$ObjectId,
+
+        [Parameter(Mandatory = $true,
+            ValueFromPipeline = $true)]
+        [string]$RoleAssignmentID,
+
+        # Tenant Domain Name.
+        [Parameter(Mandatory = $true)]
+        [string]
+        $tenantDomain,
+
+        # Api Version.
+        [Parameter(Mandatory = $false)]
+        [string]
+        $ApiVersion = '1.6'
+    )
+    
+    begin {
+        $Uri = ('https://graph.windows.net/{0}/{1}/{2}/appRoleAssignments?api-version={3}' -f $tenantDomain, 'users', $UserObjectID, $apiversion)
+        Write-Verbose -Message ('uri: {0}' -f $uri)
+    }
+    
+    process {        
+ 
+        $Body = @{
+            'id'          = $RoleAssignmentID
+            'resourceid'  = $ObjectId
+            'principalid' = $UserObjectID
+        } | ConvertTo-Json
+ 
+
+        $params = @{
+            ContentType = 'application/json'
+            Headers     = @{
+                'authorization' = "Bearer $($Token)"
+            }
+            Method      = 'Post'
+            URI         = $Uri
+            Body        = $Body
+        }
+
+        Write-Verbose -Message ($params | convertto-json)
+
+        return (Invoke-RestMethod @params)
+    }
+    
+    end {
+    }
+}
+#endregion
