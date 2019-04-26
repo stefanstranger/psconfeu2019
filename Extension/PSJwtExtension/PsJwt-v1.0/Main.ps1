75f28a63b72ef88536d71e6ed0bbe22ac3634a46
Stefan Stranger <stefan@stranger.nl>
2019-04-26 14:10:30 +0200

    Convertto-HashTable function added

--- Extension/PSJwtExtension/PsJwt-v1.0/Main.ps1
+++ Extension/PSJwtExtension/PsJwt-v1.0/Main.ps1
@@ -39,8 +39,58 @@ switch ($action) {
         Write-Verbose -Message ('Secret: {0}' -f $Secret)
         #endregion
 
+        #region Function to convert JSON to HashTable
+        #From https://4sysops.com/archives/convert-json-to-a-powershell-hash-table/
+        function ConvertTo-Hashtable {
+            [CmdletBinding()]
+            [OutputType('hashtable')]
+            param (
+                [Parameter(ValueFromPipeline)]
+                $InputObject
+            )
+        
+            process {
+                ## Return null if the input is null. This can happen when calling the function
+                ## recursively and a property is null
+                if ($null -eq $InputObject) {
+                    return $null
+                }
+        
+                ## Check if the input is an array or collection. If so, we also need to convert
+                ## those types into hash tables as well. This function will convert all child
+                ## objects into hash tables (if applicable)
+                if ($InputObject -is [System.Collections.IEnumerable] -and $InputObject -isnot [string]) {
+                    $collection = @(
+                        foreach ($object in $InputObject) {
+                            ConvertTo-Hashtable -InputObject $object
+                        }
+                    )
+        
+                    ## Return the array but don't enumerate it because the object may be pretty complex
+                    Write-Output -NoEnumerate $collection
+                }
+                elseif ($InputObject -is [psobject]) {
+                    ## If the object has properties that need enumeration
+                    ## Convert it to its own hash table and return it
+                    $hash = @{ }
+                    foreach ($property in $InputObject.PSObject.Properties) {
+                        $hash[$property.Name] = ConvertTo-Hashtable -InputObject $property.Value
+                    }
+                    $hash
+                }
+                else {
+                    ## If the object isn't an array, collection, or other object, it's already a hash table
+                    ## So just return it.
+                    $InputObject
+                }
+            }
+        }
+
+        $HashTablePayload = $PayLoad | ConvertFrom-Json | ConvertTo-HashTable
+        #endregion        
+
         Write-Host "Encode JSON Web Token"
-        ConvertTo-JWT -PayLoad $Payload -Secret $Secret         
+        ConvertTo-JWT -PayLoad $HashTablePayload -Secret $Secret         
     }
     default {
         throw 'Unknow action'
