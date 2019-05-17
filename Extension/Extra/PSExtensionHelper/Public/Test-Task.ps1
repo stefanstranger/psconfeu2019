afdd0e4c5de34722d634cd49b19902a1edc0a3a4
Stefan Stranger <stefan@stranger.nl>
2019-05-17 08:33:49 +0200

    Created new Function Test-Task

---
+++ Extension/Extra/PSExtensionHelper/Public/Test-Task.ps1
@@ -0,0 +1,17 @@
+Function Test-Task {
+    [CmdletBinding()]
+    Param
+    (# Path of Task.json file
+        [Parameter(Mandatory = $true,
+            Position = 0)]
+        $Path)
+    
+    #region Get latest task schema
+    $Schema = (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/microsoft/azure-pipelines-task-lib/master/tasks.schema.json').Content
+    #endregion
+        
+    #region validate task    
+    Test-Json -Json (Get-Content -path $Path | Out-String) -Schema $Schema -ErrorVariable jsontest -ErrorAction SilentlyContinue
+    ($jsontest.errordetails)
+    #endregion
+}
