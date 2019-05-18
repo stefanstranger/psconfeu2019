9082d142642309e53283bb7db11f92668d49c6c0
Stefan Stranger <stefan@stranger.nl>
2019-05-18 11:06:47 +0200

    Added PSHelloWorld Module to demo

--- Extension/Demo/Demo-5-ConvertToTask.ps1
+++ Extension/Demo/Demo-5-ConvertToTask.ps1
@@ -24,3 +24,27 @@ ConvertTo-Task @params -Validate
 #region open task.json
 . code "$env:Temp\task.json"
 #endregion
+
+#region Import Module PSHelloWorld
+Import-Module C:\Users\stefstr\Documents\GitHub\psconfeu2019\Extension\Extra\PSHelloWorld\Output\PSHelloWorld\0.1.0\PSHelloWorld.psd1 -Verbose
+#endregion
+
+#region variables
+$params = @{
+    Name               = 'PSHelloWorld'
+    TaskName           = 'HelloWorld-Demo'
+    Description        = 'Hello World Demo for PowerShell Conference EU 2019'
+    Category           = 'Deploy'
+    Author             = 'Stefan Stranger'
+    Version            = '1.0.0'
+    Preview            = $true
+    InstanceNameFormat = 'HelloWorld Extension Demo'
+    OutFile            = "$env:Temp\task.json"
+}
+#endregion
+
+#region Call Function ConvertTo-Task
+ConvertTo-Task @params -Validate
+#endregion
+
+. code "$env:Temp\task.json"
