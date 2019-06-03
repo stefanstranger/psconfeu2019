124e5db3d9b53e4e17c0f54cfd0a7fb7498f84db
Stefan Stranger <stefan@stranger.nl>
2019-06-03 09:59:08 +0200

    Added REST API demo for PSConfEU Agenda

--- Prototype/Demo/Demo-2-UD.ps1
+++ Prototype/Demo/Demo-2-UD.ps1
@@ -11,11 +11,11 @@
 #>
 
 #region clean op host
-Remove-Module -Name Polaris, Polaris.Class, PolarisMiddleWare.Class, PolarisRequest.Class, PolarisResponse.Class, UniversalDashboard.Community -ErrorAction SilentlyContinue
+# Remove-Module -Name Polaris, Polaris.Class, PolarisMiddleWare.Class, PolarisRequest.Class, PolarisResponse.Class, UniversalDashboard.Community -ErrorAction SilentlyContinue
 #endregion
 
 #region Import Universal Dashboard Community PowerShell Module
-Import-Module -Name 'UniversalDashboard.Community' -Verbose
+# Import-Module -Name 'UniversalDashboard.Community' -Verbose
 #endregion
 
 #region Create a REST API endpoint for processes
@@ -31,4 +31,24 @@ Invoke-RestMethod -Uri http://localhost:8686/api/process
 
 #region Stop REST API endpoint
 Stop-UDRestApi -Name 'PSConfEu'
+#endregion
+
+#region Create REST API for PSConfEU Agenda
+$Endpoint = New-UDEndpoint -Url "/psconfeu" -Method "POST" -Endpoint {
+    param($name)
+    # Get PSConfEU Agenda
+    $Agenda = Invoke-RestMethod -Method 'Get' -Uri 'https://raw.githubusercontent.com/psconfeu/2019/master/sessions.json'
+    $Agenda | 
+    Where-Object { $_.Name -match "$name" } |
+    ForEach-Object { [PSCustomObject]@{ Name = $_.Name; Starts = $_.Starts; Ends = $_.Ends; Track = $_Track; Description = $_.Description; Speaker = $_.Speaker } } | ConvertTo-Json
+}
+Start-UDRestApi -Endpoint $Endpoint -Port 8787 -Name 'Agenda'
+#endregion
+
+#region call PSConfEu Agenda REST API
+Invoke-RestMethod -Uri 'http://localhost:8787/api/psconfeu' -Method 'Post' -Body @{'Name' = 'proto' }
+#endregion
+
+#region Stop REST API endpoint
+Stop-UDRestApi -Name 'Agenda'
 #endregion
