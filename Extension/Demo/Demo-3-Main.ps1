972c8cc89ca1ea6d4836e18189d5c0569fef9cd4
Stefan Stranger <stefan@stranger.nl>
2019-06-04 13:18:55 +0200

    Added github repo info.

--- Extension/Demo/Demo-3-Main.ps1
+++ Extension/Demo/Demo-3-Main.ps1
@@ -16,9 +16,15 @@
 #>
 
 #region Import VSTS SDK Helper Module
-Import-Module C:\Users\stefstr\Documents\GitHub\psconfeu2019\Extension\ps_modules\VstsTaskSdk\VstsTaskSdk.psd1 -Verbose -Force
+Import-Module C:\Users\stefstr\Documents\GitHub\psconfeu2019\Extension\ps_modules\VstsTaskSdk\VstsTaskSdk.psd1 -whatif
+code C:\Users\stefstr\Documents\GitHub\psconfeu2019\Extension\ps_modules\VstsTaskSdk\InputFunctions.ps1
 #endregion
 
-#region Get-VSTSInput command
-Get-Command -Name Get-VstsInput -Syntax
+#region open Task library on Github
+Start-Process 'https://github.com/Microsoft/azure-pipelines-task-lib'
+Start-Process 'https://github.com/microsoft/azure-pipelines-task-lib/tree/master/powershell'
+#endregion
+
+#region PowerShell on Target Machine Task code on Github
+Start-Process 'https://github.com/microsoft/azure-pipelines-tasks/tree/master/Tasks/PowerShellOnTargetMachinesV3'
 #endregion
